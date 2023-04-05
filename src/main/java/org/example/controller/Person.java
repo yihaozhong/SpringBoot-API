36d9057c72f7a0b8e14eef9ca9e438039c6d0577
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-04-04 21:15:44 -0400

    get and post api

---
+++ src/main/java/org/example/controller/Person.java
@@ -0,0 +1,23 @@
+package org.example.controller;
+
+public class Person {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    Integer age;
+}
--- src/main/java/org/example/controller/SystemController.java
+++ src/main/java/org/example/controller/SystemController.java
@@ -1,14 +1,27 @@
 package org.example.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SystemController {
 
-    @GetMapping("/name")
-    public String getName(){
-        return "Welcome to the website";
+    @GetMapping("/name/id/{id}")
+    public String getName(@PathVariable("id") String userId){
+        if(userId.equals("1")){
+            return "Welcome to the website, 1";
+        } else if (userId.equals("2")) {
+            return "Welcome to the website, 2";
+        }
+        else{
+            return "Welcome!";
+        }
+    }
+
+
+    @PostMapping("/user")
+    public Person createUser(@RequestBody Person person){
+        person.age = person.age + 1;
+        return person;
     }
 
 }
